e04ceedfa48f51003206f36619bc614dcdd19fe8
Cerberus <shadow_sun0708@hotmail.com>
2019-08-21 19:13:48 +0700

    update hash like apollo-link-persisted-queries

--- src/utils/getOperationManifestFromProject.ts
+++ src/utils/getOperationManifestFromProject.ts
@@ -1,38 +1,56 @@
-import { GraphQLClientProject } from "apollo-language-server";
-import {
-  defaultOperationRegistrySignature,
-  operationHash
-} from "apollo-graphql";
+import { GraphQLClientProject } from 'apollo-language-server'
+import { defaultOperationRegistrySignature } from 'apollo-graphql'
+
+import sha256 from 'hash.js/lib/hash/sha/256'
+import { addTypenameToDocument } from 'apollo-utilities'
+
+import { print } from 'graphql/language/printer'
+import { DocumentNode } from 'graphql'
 
 export interface ManifestEntry {
-  signature: string;
-  document: string;
-  metadata: {
-    engineSignature: string;
-  };
+	signature: string
+	document: string
+	metadata: {
+		engineSignature: string
+	}
 }
 
+export const sortDefinitions = (document: DocumentNode) => ({
+	...document,
+	// @ts-ignore there is a sort property inside
+	definitions: document.definitions.sort((a, b) => {
+		if (a.name.value < b.name.value) return -1
+		if (a.name.value > b.name.value) return 1
+		return 0
+	}),
+})
+
 export function getOperationManifestFromProject(
-  project: GraphQLClientProject
+	project: GraphQLClientProject,
 ): ManifestEntry[] {
-  const manifest = Object.entries(
-    project.mergedOperationsAndFragmentsForService
-  ).map(([operationName, operationAST]) => {
-    const printed = defaultOperationRegistrySignature(
-      operationAST,
-      operationName
-    );
+	const manifest = Object.entries(
+		project.mergedOperationsAndFragmentsForService,
+	).map(([operationName, operationAST]) => {
+		const sorted = sortDefinitions(operationAST)
+		const operationASTwithTypename = addTypenameToDocument(sorted)
+		const printed = defaultOperationRegistrySignature(
+			operationASTwithTypename,
+			operationName,
+		)
+		const signature = sha256()
+			.update(print(operationAST))
+			.digest('hex')
 
-    return {
-      signature: operationHash(printed),
-      document: printed,
-      // TODO: unused. Remove or repurpose this field altogether with op. registry 2.0 work.
-      // For now, this field is non-nullable on the input type.
-      metadata: {
-        engineSignature: ""
-      }
-    };
-  });
+		return {
+			signature,
+			document: printed,
+			// TODO: unused. Remove or repurpose this field altogether with op. registry 2.0 work.
+			// For now, this field is non-nullable on the input type.
+			metadata: {
+				engineSignature: '',
+			},
+		}
+	})
 
-  return manifest;
+	return manifest
 }
